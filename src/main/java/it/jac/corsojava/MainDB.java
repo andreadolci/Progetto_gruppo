12d1b8616c07fb9001e599e22f623eb53960baa5
13Febo <federico11bonomi@gmail.com>
2023-04-03 11:08:59 +0200

    Andrea

--- src/main/java/it/jac/corsojava/MainDB.java
+++ src/main/java/it/jac/corsojava/MainDB.java
@@ -11,7 +11,7 @@ public class MainDB {
 	
 	public static void main(String[] args) {
 
-		log.info("Applicazione iniziata");
+		log.info("Andrea coglione");
 		
 		UserService.getInstance().create(
 			"armiespo", "armando", "esposito", "123");
